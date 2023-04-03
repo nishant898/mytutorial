a124c18a2e32103f069e3caa25652a87978b7df9
nishant898 <122629078+nishant898@users.noreply.github.com>
2023-04-03 22:31:42 +0530

    Add files via upload

---
+++ transversal operation.c
@@ -0,0 +1,16 @@
+//C program to Implement a List using Array and develop function to perform Traversal operation
+// header files
+#include <stdio.h>
+//main function
+void main() {
+//array declaration
+int Arr[5] = {18, 30, 15, 70, 12};
+int i;
+//output statement
+printf("Elements of the array are:\n");
+//using for statement
+for(i = 0; i<5; i++) {
+//output statement
+printf("Arr[%d] = %d,  ", i, Arr[i]);
+}
+}
